2e6d3217f626d84fe368675611c10941c40bcd99
bhoettges <bhoettges77@gmail.com>
2021-05-12 18:30:43 +0200

    addexpense view

---
+++ SpendingManagementTool/AddExpenseViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AddExpenseViewController.swift
+//  SpendingManagementTool
+//
+//  Created by Balthasar Hoettges on 12/05/2021.
+//
+
+import UIKit
+
+class AddExpenseViewController: UIViewController {
+
+//    @IBOutlet weak var textFieldAmount: UITextField!
+//    @IBOutlet weak var textFieldNotes: UITextField!
+//    @IBOutlet weak var DatePicker: UIDatePicker!
+//    @IBOutlet weak var OccurenceControl: UISegmentedControl!
+//    @IBOutlet weak var ToggleCalendarSwitch: UISwitch!
+//
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+   
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
